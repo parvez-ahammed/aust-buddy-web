11d4940316636ffe0f3c553627211dc6d6fbcb4f
piru72 <parvezdirom2000@gmail.com>
2023-07-29 18:43:26 +0600

    fix(views) : connected with the navbar

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace AUST_BUDDY_WEB.Controllers
+{
+	public class AuthenticationController : Controller
+	{
+		// GET: Authentication
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult Login()
+		{
+			ViewBag.Message = "Your login page.";
+			return View();
+		}
+
+		public ActionResult Registration()
+		{
+			ViewBag.Message = "Your Registration page.";
+			return View();
+		}
+	}
+}
