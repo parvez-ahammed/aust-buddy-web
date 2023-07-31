dbdfafcdc423a550658a09ebe9e1a9a94bd70319
piru72 <parvezdirom2000@gmail.com>
2023-08-01 01:04:08 +0600

    final before showcase feature(index) : page created

--- Controllers/AuthenticationController.cs
+++ Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using AUST_BUDDY_WEB.Models;
+using System.Web.Mvc;
 
 namespace AUST_BUDDY_WEB.Controllers
 {
@@ -17,6 +18,13 @@ namespace AUST_BUDDY_WEB.Controllers
 		}
 
 		public ActionResult Registration()
+		{
+			ViewBag.Message = "Your Registration page.";
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Registration(UserAll model)
 		{
 			ViewBag.Message = "Your Registration page.";
 			return View();
