f32fa7912103187240911e127cfbac2252162afa
piru72 <parvezdirom2000@gmail.com>
2023-08-15 03:40:41 +0600

    feat (announcement) : added announcement

--- Controllers/AnnouncementController.cs
+++ Controllers/AnnouncementController.cs
@@ -1,18 +1,62 @@
-﻿using System.Web.Mvc;
+﻿using AUST_BUDDY_WEB.Features.Course;
+using AUST_BUDDY_WEB.Models;
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using FireSharp.Interfaces;
+using System.Web.Mvc;
 
 namespace AUST_BUDDY_WEB.Controllers
 {
 	public class AnnouncementController : Controller
 	{
-		// GET: Announcement
-		public ActionResult Index()
+        private readonly IFirebaseConfig _firebaseConfig;
+        private readonly IFirebaseClient _firebaseClient;
+        // GET: Announcement
+        public ActionResult Index()
 		{
 			return View();
 		}
+        public AnnouncementController()
+        {
+            _firebaseConfig = new FirebaseConfig
+            {
+                AuthSecret = "fbs0BK9VcSR6Y2xZ6CSjA0JIK1S8yBCaVFQ00aXK",
+                BasePath = "https://aust-buddies-default-rtdb.asia-southeast1.firebasedatabase.app"
+            };
+            _firebaseClient = new FireSharp.FirebaseClient(_firebaseConfig);
+        }
 
-		public ActionResult Announcement()
+        private List<Announcement> GetCAnnouncementDataFromFirebase(string path)
+        {
+            // Replace "courses" with the path to your Firebase collection where course data is stored.
+            FirebaseResponse response = _firebaseClient.Get(path);
+
+            // Check if the data is null or not found
+            if (response.Body == "null")
+            {
+                return new List<Announcement>();
+            }
+
+            var announcement = response.ResultAs<Dictionary<string, Announcement>>();
+            return announcement.Values.ToList();
+        }
+
+        public ActionResult Announcement()
 		{
-			return View();
+
+            List<Announcement> announcementList;
+            string path = "public-announcements/all";
+            announcementList = GetCAnnouncementDataFromFirebase(path);
+            return View(announcementList);
+            
 		}
 		public ActionResult Add()
 		{
--- Controllers/credentials.cs
+++ Controllers/credentials.cs
@@ -1,7 +1,12 @@
-﻿namespace AUST_BUDDY_WEB.Controllers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AUST_BUDDY_WEB.Controllers
 {
-	public class credentials
-	{
-		public const string youTubeApi = "AIzaSyCluY46Az24q7XCfAvQihtEOMY1GfHxnWQ";
-	}
+    public class credentials
+    {
+        public const string youTubeApi = "AIzaSyCluY46Az24q7XCfAvQihtEOMY1GfHxnWQ";
+    }
 }
