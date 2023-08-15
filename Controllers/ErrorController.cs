745d71ae6bbd290497808307a64ff8df32a2756c
piru72 <parvezdirom2000@gmail.com>
2023-08-15 06:02:54 +0600

    add (error) : error page added

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace AUST_BUDDY_WEB.Controllers
+{
+	public class ErrorController : Controller
+	{
+		// GET: Error
+		public ActionResult Index()
+		{
+			return View("Error");
+		}
+
+		public ActionResult NotFound()
+		{
+			Response.StatusCode = 404;
+			return View("NotFound");
+		}
+	}
+}
